906824eea767261dc8cc8bef506880d98d7e3d48
Cory Thompson <coryjthompson@gmail.com>
2014-01-20 20:36:30 +1100

    Moved Copy clipboard items into user32 wrapper

--- bclip/Windows/User32Wrapper.cs
+++ bclip/Windows/User32Wrapper.cs
@@ -10,9 +10,6 @@ namespace bclip.Windows
         #region Constants 
 
         /*defined in winuser.h*/
-        //get/set text of an element
-        private const int WM_GETTEXT = 0x0D;
-        private const int WM_SETTEXT = 0x000c;
 
         //clipboard related
         private const int WM_DRAWCLIPBOARD = 0x308;
@@ -24,6 +21,13 @@ namespace bclip.Windows
         #endregion Constants
 
         #region External Functions
+
+        [DllImport("User32.dll")]
+        private static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll")]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
 
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
         private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
@@ -38,10 +42,13 @@ namespace bclip.Windows
         private static extern bool GetGUIThreadInfo(int tId, out GuiThreadInfo threadInfo);
 
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, System.Text.StringBuilder lParam);
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, StringBuilder lParam);
 
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
         public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, string lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
         public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
@@ -50,18 +57,28 @@ namespace bclip.Windows
 
         #region EventHandlers
 
-        private EventHandler OnHotKeyPressed;
+        private EventHandler _onDrawClipboard;
+        private EventHandler _onHotKeyPressed;
 
         #endregion EventHandlers
 
+        private IntPtr _nextClipboardViewer;
+
+        public void RegisterClipboard(EventHandler onDrawClipboard)
+        {
+            _onDrawClipboard = onDrawClipboard;
+            _nextClipboardViewer = (IntPtr) SetClipboardViewer((int) Handle);
+        }
+
         private static GuiThreadInfo GetThreadInfo(int tid)
         {
-            GuiThreadInfo tinfo = new GuiThreadInfo();
-            tinfo.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(tinfo);
+            var tinfo = new GuiThreadInfo();
+            tinfo.cbSize = Marshal.SizeOf(tinfo);
             GetGUIThreadInfo(tid, out tinfo);
             return tinfo;
         }
 
+        /*
         public static string FocusedClassName()
         {
             GuiThreadInfo info = GetThreadInfo((int) 0);
@@ -69,49 +86,41 @@ namespace bclip.Windows
             var x = GetClassName(info.hwndCapred, className, 100);
             return className.ToString();
 
-        }
+        }*/
 
         public static IntPtr FocusedHandle()
         {
-            GuiThreadInfo info = GetThreadInfo((int) 0);
+            GuiThreadInfo info = GetThreadInfo(0);
             return info.hwndFocus;
-
-        }
-
-        public static string GetText()
-        {
-            IntPtr focusedHandle = FocusedHandle();
-            System.Text.StringBuilder sb = new System.Text.StringBuilder(255); // or length from call with GETTEXTLENGTH
-
-            SendMessage(focusedHandle, WM_GETTEXT, sb.Capacity, sb);
-            return sb.ToString();
-
-        }
-
-        public static void SetText()
-        {
-            IntPtr focusedHandle = FocusedHandle();
-            StringBuilder sb = new StringBuilder(255);
-            //sb.Append("Hello there");
-            SendMessage(focusedHandle, WM_SETTEXT, IntPtr.Zero, "Hello World");
         }
 
         public new void Dispose()
         {
-            UnregisterHotKey(this.Handle, 1);
+            UnregisterHotKey(Handle, 1);
+            ChangeClipboardChain(Handle, _nextClipboardViewer);
             base.Dispose();
-            //UnRegister hot key here.   
         }
 
-        protected override void WndProc(ref System.Windows.Forms.Message m)
+        protected override void WndProc(ref Message m)
         {
             switch (m.Msg)
             {
-
                 case WM_HOTKEY:
-
-                    this.OnHotKeyPressed(this, new EventArgs());
+                    _onHotKeyPressed(this, new EventArgs());
                     break;
+
+                case WM_DRAWCLIPBOARD:
+                    _onDrawClipboard(this, new EventArgs());
+                    SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == _nextClipboardViewer)
+                        _nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
 
                 default:
                     base.WndProc(ref m);
@@ -121,35 +130,35 @@ namespace bclip.Windows
 
         public void RegisterHotKey(int id, int modifiers, int keys, EventHandler onHotKeyPressed)
         {
-            if (OnHotKeyPressed != null)
+            if (_onHotKeyPressed != null)
                 throw new NotImplementedException("Currently unable to register multiple hot keys");
 
-            OnHotKeyPressed = onHotKeyPressed;
-            RegisterHotKey(this.Handle, id, modifiers, keys);
+            _onHotKeyPressed = onHotKeyPressed;
+            RegisterHotKey(Handle, id, modifiers, keys);
         }
 
         #region DataStructures
-
-        public struct RECT
-        {
-            public uint left;
-            public uint top;
-            public uint right;
-            public uint bottom;
-        }
 
         public struct GuiThreadInfo
         {
             public int cbSize;
             public uint flags;
             public IntPtr hwndActive;
-            public IntPtr hwndFocus;
+            public IntPtr hwndCapred;
             public IntPtr hwndCapture;
+            public IntPtr hwndFocus;
             public IntPtr hwndMenuOwner;
             public IntPtr hwndMoveSize;
-            public IntPtr hwndCapred;
             public RECT rcCaret;
         };
+
+        public struct RECT
+        {
+            public uint bottom;
+            public uint left;
+            public uint right;
+            public uint top;
+        }
 
         #endregion DataStructures
     }
--- bclip/Windows/WindowsClipboard.cs
+++ bclip/Windows/WindowsClipboard.cs
@@ -1,91 +1,33 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Forms;
 using bclip.Model;
-using bclip.Windows;
+
 
 namespace bclip.Windows
  
 {
     class WindowsClipboard:ClipboardBase
     {
-        #region fields
+        #region constants
         public static int KEY_V = 0x0056;
         public static int MOD_CONTROL = 0x0002;
         #endregion
-     
-        
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("User32.dll")]
-        protected static extern int SetClipboardViewer(int hWndNewViewer);
-
-        [DllImport("User32.dll")]
-        public static extern bool ChangeClipboardChain(IntPtr hWndRemove,IntPtr hWndNewNext);
-
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam,IntPtr lParam);
-        
-        [DllImport("User32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
-        
-        [DllImport("user32.dll")]
-        static extern bool GetGUIThreadInfo(uint idThread, ref User32Wrapper lpgui);
-        IntPtr nextClipboardViewer;
-        private User32Wrapper.GuiThreadInfo guiInfo;
 
         public WindowsClipboard()
         {
-            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
             User32Wrapper user32Wrapper = new User32Wrapper();
             user32Wrapper.RegisterHotKey(1, MOD_CONTROL, KEY_V, OnHotKeyPressed);
+            user32Wrapper.RegisterClipboard(OnDrawClipboard);
         }
 
         public void OnHotKeyPressed(object sender, EventArgs e)
         {
             OnPasteDetected();
         }
-   
 
-        protected override void Dispose(bool disposing)
+        public void OnDrawClipboard(object sender, EventArgs e)
         {
-            ChangeClipboardChain(this.Handle, nextClipboardViewer);
-            base.Dispose(disposing);
-        }
-        
-        protected override void WndProc(ref System.Windows.Forms.Message m)
-        {
-            // defined in winuser.h
-            const int WM_DRAWCLIPBOARD = 0x308;
-            const int WM_CHANGECBCHAIN = 0x030D;
-
-            switch (m.Msg)
-            {
-       
-                case WM_DRAWCLIPBOARD:
-                    CopyItem copyItem = CopyItem.FromClipboard();
-                    this.OnCopyDetected(copyItem);
-
-                    SendMessage(nextClipboardViewer, m.Msg, m.WParam,m.LParam);
-                    break;
-
-                case WM_CHANGECBCHAIN:
-                    if (m.WParam == nextClipboardViewer)
-                        nextClipboardViewer = m.LParam;
-                    else
-                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
-                    
-                    break;
-
-                default:
-                    base.WndProc(ref m);
-                    break;
-            }
-            
+            CopyItem copyItem = CopyItem.FromClipboard();
+            OnCopyDetected(copyItem);
         }
     }
 }
